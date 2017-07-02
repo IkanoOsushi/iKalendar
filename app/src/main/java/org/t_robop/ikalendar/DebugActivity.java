23b673f575edd8f3be7057f7c425a995c58722a6
BuildTools <unconfigured@null.spigotmc.org>
2017-07-03 01:39:04 +0900

    各アクティビティにNavigation Drawer追加

--- app/src/main/java/org/t_robop/ikalendar/DebugActivity.java
+++ app/src/main/java/org/t_robop/ikalendar/DebugActivity.java
@@ -1,8 +1,14 @@
 package org.t_robop.ikalendar;
 
 import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,21 +17,33 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 
-public class DebugActivity extends AppCompatActivity {
+public class DebugActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     ListView listView;
     ArrayAdapter arrayAdapter;
     ArrayList<Class> arrayList;
 
-
     //ここに入れればデバッグアクティビティのリストに追加される
     Class[] DebugClass={CalendarActivity.class,MainActivity.class,ReminderActivity.class,TimetableActivity.class};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //ここからNavigation Drawerのやつ
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        //ここまでNavigation Drawerのやつ
 
         listView = (ListView) findViewById(R.id.list);
         arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
@@ -43,10 +61,32 @@ public class DebugActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
     }
 
+    //Navigation Drawer内のメニューを押した時の動作
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
 
+        if (id == R.id.nav_main) {
+            Intent intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_calendar) {
+            Intent intent = new Intent(this,CalendarActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_timetable) {
+            Intent intent = new Intent(this,TimetableActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_reminder) {
+            Intent intent = new Intent(this,ReminderActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_setting) {
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
 
