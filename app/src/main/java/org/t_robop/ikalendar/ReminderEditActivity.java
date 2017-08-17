d114a341deff2b0b40c9739f51a35335228eb8d4
Dai1678 <dai7774@gmail.com>
2017-08-17 18:12:57 +0900

    getIntentの準備中

--- app/src/main/java/org/t_robop/ikalendar/ReminderEditActivity.java
+++ app/src/main/java/org/t_robop/ikalendar/ReminderEditActivity.java
@@ -18,123 +18,31 @@ public class ReminderEditActivity extends AppCompatActivity {
 
     EditText editText;
     TextView time;
-    String note_id = null;
+    String getIntentTime = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_edit);
 
-        //   String note = getIntent().getStringExtra("note");
-        //   Log.d("aaaaa",note);
-
+        getIntentTime = getIntent().getStringExtra("time");     //ReminderActivityでタップされたListの時間を取得
 
         editText = (EditText) findViewById(R.id.edit);
         time = (TextView) findViewById(R.id.EditNowTime);
 
-        note_id = getIntent().getStringExtra("note_id");
-
-        String digitime = "0:00";
-        switch (note_id) {
-            case "note0000":
-                digitime = "00:00";
-                break;
-            case "note0100":
-                digitime = "01:00";
-                break;
-            case "note0200":
-                digitime = "02:00";
-                break;
-            case "note0300":
-                digitime = "03:00";
-                break;
-            case "note0400":
-                digitime = "04:00";
-                break;
-            case "note0500":
-                digitime = "05:00";
-                break;
-            case "note0600":
-                digitime = "06:00";
-                break;
-            case "note0700":
-                digitime = "07:00";
-                break;
-            case "note0800":
-                digitime = "08:00";
-                break;
-            case "note0900":
-                digitime = "09:00";
-                break;
-            case "note1000":
-                digitime = "10:00";
-                break;
-            case "note1100":
-                digitime = "11:00";
-                break;
-            case "note1200":
-                digitime = "12:00";
-                break;
-            case "note1300":
-                digitime = "13:00";
-                break;
-            case "note1400":
-                digitime = "14:00";
-                break;
-            case "note1500":
-                digitime = "15:00";
-                break;
-            case "note1600":
-                digitime = "16:00";
-                break;
-            case "note1700":
-                digitime = "17:00";
-                break;
-            case "note1800":
-                digitime = "18:00";
-                break;
-            case "note1900":
-                digitime = "19:00";
-                break;
-            case "note2000":
-                digitime = "20:00";
-                break;
-            case "note2100":
-                digitime = "21:00";
-                break;
-            case "note2200":
-                digitime = "22:00";
-                break;
-            case "note2300":
-                digitime = "23:00";
-                break;
-
-
-        }
-        time.setText(String.valueOf(digitime));
+        time.setText(String.valueOf(getIntentTime));
 
     }
 
-    public void cancel(View v) {
+    public void cancelClick(View v) {
         finish();
-        /*
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("note","");
-        startActivity(intent);
-        */
     }
-    public void add2(View v) {
-        String text = editText.getText().toString();
-        String note = getIntent().getStringExtra("note");
+    public void addClick(View v) {
+        String text = editText.getText().toString();    //editした予定(文字列)を取得
 
-        switch (note_id) {
-            case "note0000":
-                Intent intent = new Intent(this, ReminderActivity.class);
-                intent.putExtra("note",text);
-                intent.putExtra("note_id",note_id);
-                startActivity(intent);
-                break;
-        }
+        Intent intent = new Intent(this, ReminderActivity.class);
+        intent.putExtra("note",text);   //予定(文字列)
+        startActivity(intent);
 
     }
 }
