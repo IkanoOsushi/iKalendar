7f402b2924d99a32abb1fd84e21750502ef3c948
t-uranus <t.urano1102@gmail.com>
2017-08-17 18:09:44 +0900

    onItemListenerを追加しました。

--- app/src/main/java/org/t_robop/ikalendar/ReminderEditActivity.java
+++ app/src/main/java/org/t_robop/ikalendar/ReminderEditActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -28,89 +29,13 @@ public class ReminderEditActivity extends AppCompatActivity {
         //   String note = getIntent().getStringExtra("note");
         //   Log.d("aaaaa",note);
 
-
         editText = (EditText) findViewById(R.id.edit);
         time = (TextView) findViewById(R.id.EditNowTime);
 
-        note_id = getIntent().getStringExtra("note_id");
+        //note_id = getIntent().getStringExtra("note_id");
 
         String digitime = "0:00";
-        switch (note_id) {
-            case "note0000":
-                digitime = "00:00";
-                break;
-            case "note0100":
-                digitime = "01:00";
-                break;
-            case "note0200":
-                digitime = "02:00";
-                break;
-            case "note0300":
-                digitime = "03:00";
-                break;
-            case "note0400":
-                digitime = "04:00";
-                break;
-            case "note0500":
-                digitime = "05:00";
-                break;
-            case "note0600":
-                digitime = "06:00";
-                break;
-            case "note0700":
-                digitime = "07:00";
-                break;
-            case "note0800":
-                digitime = "08:00";
-                break;
-            case "note0900":
-                digitime = "09:00";
-                break;
-            case "note1000":
-                digitime = "10:00";
-                break;
-            case "note1100":
-                digitime = "11:00";
-                break;
-            case "note1200":
-                digitime = "12:00";
-                break;
-            case "note1300":
-                digitime = "13:00";
-                break;
-            case "note1400":
-                digitime = "14:00";
-                break;
-            case "note1500":
-                digitime = "15:00";
-                break;
-            case "note1600":
-                digitime = "16:00";
-                break;
-            case "note1700":
-                digitime = "17:00";
-                break;
-            case "note1800":
-                digitime = "18:00";
-                break;
-            case "note1900":
-                digitime = "19:00";
-                break;
-            case "note2000":
-                digitime = "20:00";
-                break;
-            case "note2100":
-                digitime = "21:00";
-                break;
-            case "note2200":
-                digitime = "22:00";
-                break;
-            case "note2300":
-                digitime = "23:00";
-                break;
 
-
-        }
         time.setText(String.valueOf(digitime));
 
     }
@@ -125,7 +50,7 @@ public class ReminderEditActivity extends AppCompatActivity {
     }
     public void add2(View v) {
         String text = editText.getText().toString();
-        String note = getIntent().getStringExtra("note");
+        //String note = getIntent().getStringExtra("note");
 
         switch (note_id) {
             case "note0000":
