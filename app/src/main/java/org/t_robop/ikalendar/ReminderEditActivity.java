a0aa49c6cad2d81b6cc8b859c33b09c13dc9e2ca
t-uranus <t.urano1102@gmail.com>
2017-08-23 13:51:10 +0900

    EditActivityレイアウトのズレを解消しました。 画面を縦画面に固定しました。（後にリスト画面も固定化します）

--- app/src/main/java/org/t_robop/ikalendar/ReminderEditActivity.java
+++ app/src/main/java/org/t_robop/ikalendar/ReminderEditActivity.java
@@ -1,6 +1,7 @@
 package org.t_robop.ikalendar;
 
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,7 +18,7 @@ public class ReminderEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder_edit);
-
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         String getIntentTime = getIntent().getStringExtra("time");     //ReminderActivityでタップされたListの時間を取得
         Intent intent =getIntent();
         String note = intent.getStringExtra("note");
